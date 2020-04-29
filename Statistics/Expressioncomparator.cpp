f7aebcc3c0cffe8532b962a41a0b37d4a06d3e0a
Numelen <nonokat@pm.me>
2020-04-29 23:07:10 +0200

    Standard cluster wise correlation with Zero Values enabled.
    Reimplemented the correlation method with zero values enabled.

--- Statistics/Expressioncomparator.cpp
+++ Statistics/Expressioncomparator.cpp
@@ -27,36 +27,103 @@ QVector<QVector<QPair<QString, double>>> findClusterTissueCorrelations(QVector<F
     QVector<QVector<QPair<QString, double>>> tissueCorrelationsForAllClusters;
     tissueCorrelationsForAllClusters.reserve(clusters.length());
 
+    // For every cluster
     for (int i = 0; i < clusters.length(); i++) {
-        QVector<QPair<QString, double>> clusterTissueCorrelations;
-        clusterTissueCorrelations.reserve(tissues.length());
+        QVector<QPair<QString, double>> tissueClusterCorrelations;
+        tissueClusterCorrelations.reserve(tissues.length());
 
+        // go through every tissue type
         for (int j = 0; j < tissues.length(); j++) {
-            QVector<QPair<Feature, Feature>> equallyExpressedFeatures = Sorter::findEquallyExpressedFeatures(clusters[i], tissues[j]);
 
-            int numberOfEquallyExpressedFeatures = equallyExpressedFeatures.length();
-
+            // Create a list that will hold all expression counts of both the current tissue j and the current cluster i
             QVector<double> tissueFeatureExpressionCounts,
                             clusterFeatureExpressionCounts;
-            tissueFeatureExpressionCounts.reserve(numberOfEquallyExpressedFeatures);
-            clusterFeatureExpressionCounts.reserve(numberOfEquallyExpressedFeatures);
-            for (QPair<Feature, Feature> equallyExpressedFeature : equallyExpressedFeatures) {
-                clusterFeatureExpressionCounts.append(equallyExpressedFeature.first.count);
-                tissueFeatureExpressionCounts.append(equallyExpressedFeature.second.count);
+
+            // And for every feature in the tissue j
+            for (int k = 0; k < clusters[i].getNumberOfFeatures(); k++) {
+
+                // Grab the expression count of the current looked at feature k in the current tissue j
+                double tissueFeatureExpressionCount = clusters[i].getFeatureExpressionCount(k);
+
+                // And add it to the list
+                clusterFeatureExpressionCounts.append(tissueFeatureExpressionCount);
+
+                bool isFeatureAlsoExpressedInTissue = false;
+
+                // go through every feature in the current cluster i and search for the current looked at tissue feature
+                for (int l = 0; l < tissues[j].getNumberOfFeatures(); l++) {
+
+                    // for this check whether the feature IDs are equal
+                    bool isFeatureIDEqual = tissues[j].getFeatureID(l).compare(clusters[i].getFeatureID(k)) == 0;
+
+                    // If so, this means the feature has been found in the current cluster and the feature is expressed in the cluster
+                    if (isFeatureIDEqual) {
+                        isFeatureAlsoExpressedInTissue = true;
+
+                        // So add its expression count to the list
+                        double tissueFeatureExpressionCount = tissues[j].getFeatureExpressionCount(l);
+
+                        tissueFeatureExpressionCounts.append(tissueFeatureExpressionCount);
+
+                        // And end the search for it in the current cluster
+                        break;
+                    }
+                }
+
+                // If the feature has not been found in the current cluster, it means it is not expressed
+                // In this case, append 0 as the feature expression count to the list
+                if (!isFeatureAlsoExpressedInTissue) {
+                    tissueFeatureExpressionCounts.append(.0);
+                }
             }
 
-            double correlation = Correlator::calculateSpearmanCorrelation(clusterFeatureExpressionCounts, tissueFeatureExpressionCounts);
+            // If all features with their correct expression counts have been found and aligned in the two lists, calculate the correlation
+            double tissueClusterCorrelation = Correlator::calculateSpearmanCorrelation(tissueFeatureExpressionCounts, clusterFeatureExpressionCounts);
 
-            clusterTissueCorrelations.append(qMakePair(tissues[j].ID, correlation));
+            // And add it to the correlations list
+            tissueClusterCorrelations.append(qMakePair(tissues[j].ID, tissueClusterCorrelation));
         }
 
-        std::sort(clusterTissueCorrelations.begin(), clusterTissueCorrelations.end(),
+        // In the end, sort the found tissue correlations after the correlation coefficients
+        std::sort(tissueClusterCorrelations.begin(), tissueClusterCorrelations.end(),
                   [](QPair<QString, double> pairA, QPair<QString, double> pairB) { return pairA.second > pairB.second; });
 
-        tissueCorrelationsForAllClusters.append(clusterTissueCorrelations);
+        // and add the sorted list to the list of clusters with correlations to all tissues
+        tissueCorrelationsForAllClusters.append(tissueClusterCorrelations);
     }
 
     return tissueCorrelationsForAllClusters;
+
+//    for (int i = 0; i < clusters.length(); i++) {
+//        QVector<QPair<QString, double>> clusterTissueCorrelations;
+//        clusterTissueCorrelations.reserve(tissues.length());
+
+//        for (int j = 0; j < tissues.length(); j++) {
+//            QVector<QPair<Feature, Feature>> equallyExpressedFeatures = Sorter::findEquallyExpressedFeatures(clusters[i], tissues[j]);
+
+//            int numberOfEquallyExpressedFeatures = equallyExpressedFeatures.length();
+
+//            QVector<double> tissueFeatureExpressionCounts,
+//                            clusterFeatureExpressionCounts;
+//            tissueFeatureExpressionCounts.reserve(numberOfEquallyExpressedFeatures);
+//            clusterFeatureExpressionCounts.reserve(numberOfEquallyExpressedFeatures);
+//            for (QPair<Feature, Feature> equallyExpressedFeature : equallyExpressedFeatures) {
+//                clusterFeatureExpressionCounts.append(equallyExpressedFeature.first.count);
+//                tissueFeatureExpressionCounts.append(equallyExpressedFeature.second.count);
+//            }
+
+//            double correlation = Correlator::calculateSpearmanCorrelation(clusterFeatureExpressionCounts, tissueFeatureExpressionCounts);
+
+//            clusterTissueCorrelations.append(qMakePair(tissues[j].ID, correlation));
+//        }
+
+//        std::sort(clusterTissueCorrelations.begin(), clusterTissueCorrelations.end(),
+//                  [](QPair<QString, double> pairA, QPair<QString, double> pairB) { return pairA.second > pairB.second; });
+
+//        tissueCorrelationsForAllClusters.append(clusterTissueCorrelations);
+//    }
+
+//    return tissueCorrelationsForAllClusters;
 }
 
 
